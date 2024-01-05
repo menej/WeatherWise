54132da1b995be106b6dc2e71a65f2acde5e2487
Enej <68961385+menej@users.noreply.github.com>
2024-01-05 21:46:14 +0100

    feat: Add a utility class

---
+++ WeatherWise/Utilities/CityCoordinates.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherAnalyzer;
+
+public class CityCoordinates
+{
+    public static readonly Dictionary<string, (double Latitude, double Longitude)> coordinates = new()
+    {
+        { "Ljubljana", (46.056946, 14.505752) },
+        { "Kranj", (46.23887, 14.35561) },
+        { "Koper", (45.548059, 13.730188) },
+    };
+}
