2f8921be9f6b7020ac5b8aacb60a272d75d19a8e
Enej <68961385+menej@users.noreply.github.com>
2024-01-05 22:10:11 +0100

    feat: Add weather service

---
+++ WeatherWise/Services/WeatherService.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherWise.Services;
+
+public class WeatherService
+{
+    public static void DisplayTemperature()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DisplayHumidity()
+    {
+        throw new NotImplementedException();
+    }
+}
