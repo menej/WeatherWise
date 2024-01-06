93116cb3ddde6defc7f5e8c8de48f83fbb3930a1
Enej <68961385+menej@users.noreply.github.com>
2024-01-06 07:02:08 +0100

    feat: Add corresponding methods for main menu functions and implement a simple functionality for choosing location

--- WeatherWise/Utilities/ConsoleMenu.cs
+++ WeatherWise/Utilities/ConsoleMenu.cs
@@ -14,7 +14,7 @@ public class ConsoleMenu
     public void DisplayMainMenu()
     {
         DisplayWelcomeMessage();
-        
+
         while (true)
         {
             Console.WriteLine("Please choose an option:");
@@ -23,30 +23,36 @@ public class ConsoleMenu
             Console.WriteLine("3. Sunrise and sunset");
             Console.WriteLine("4. Precipitation probability");
             Console.WriteLine("5. Wind info");
-            
+
             if (AppUser.DefaultLocation == null) Console.WriteLine("6. Set default location");
             else Console.WriteLine("6. Change or unset default location");
-            
+
             Console.WriteLine("7. Exit application");
             Console.WriteLine();
 
             Console.Write("Enter option: ");
-            
+
             string? option = Console.ReadLine();
-            
+
             switch (option)
             {
                 case "1":
+                    DisplayGeneralInfo();
                     break;
                 case "2":
+                    DisplayTemperatureAndHumidity();
                     break;
                 case "3":
+                    DisplaySunriseAndSunset();
                     break;
                 case "4":
+                    DisplayPrecipitationProbability();
                     break;
                 case "5":
+                    DisplayWindInfo();
                     break;
                 case "6":
+                    SetOrChangeDefaultLocation();
                     break;
                 case "7":
                     return;
@@ -55,6 +61,85 @@ public class ConsoleMenu
                     break;
             }
         }
+    }
+
+    private void DisplayTemperatureAndHumidity()
+    {
+        var userCity = ChooseLocation();
+        if (userCity.Equals("back"))
+        {
+            return;
+        }
+
+        Console.WriteLine("Displaying temperature and humidity.");
+        Console.WriteLine();
+    }
+
+    private static string? ChooseLocation()
+    {
+        var cities = CityCoordinates.coordinates;
+        var cityNames = cities.Keys;
+
+        string? userChoice = null;
+
+        while (userChoice == null)
+        {
+            Console.WriteLine("Available cities: " + string.Join(", ", cityNames));
+            Console.WriteLine("Type \"back\" to return to main menu.");
+            Console.Write("Please enter a City from the available list: ");
+
+            var userInput = Console.ReadLine();
+
+            if (userInput is "back" or "\"back\"")
+            {
+                return null;
+            }
+            
+            var matchedCity =
+                cityNames.FirstOrDefault(city => city.Equals(userInput, StringComparison.OrdinalIgnoreCase));
+
+            if (matchedCity == null)
+            {
+                Console.WriteLine("Please provide a city that is on the list.\n");
+                continue;
+            } 
+
+            userChoice = matchedCity;
+            Console.WriteLine($"You have selected: {userChoice}");
+        }
+        
+        if (userChoice == null)
+        {
+            throw new InvalidOperationException("User choice should never be null here.");
+        }
+        
+        return userChoice;
+    }
+
+    private void SetOrChangeDefaultLocation()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DisplayWindInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DisplayPrecipitationProbability()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DisplaySunriseAndSunset()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private void DisplayGeneralInfo()
+    {
+        throw new NotImplementedException();
     }
 
     private static void DisplayWelcomeMessage()
