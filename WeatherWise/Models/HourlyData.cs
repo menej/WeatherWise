fe3b2cdfa8da23d786a1e284020bfe949dc70427
Enej <68961385+menej@users.noreply.github.com>
2024-01-07 14:01:55 +0100

    feat: Update DTOs properties

--- WeatherWise/Models/HourlyData.cs
+++ WeatherWise/Models/HourlyData.cs
@@ -5,8 +5,14 @@ namespace WeatherWise.Models;
 public class HourlyData
 {
     [JsonPropertyName("time")]
-    public string[] Time { get; set; }
+    public string[]? Time { get; set; }
 
     [JsonPropertyName("temperature_2m")]
-    public double[] Temperature_2m { get; set; }
+    public double[]? Temperature2M { get; set; }
+    
+    [JsonPropertyName("apparent_temperature")]
+    public double[]? ApparentTemperature { get; set; }
+    
+    [JsonPropertyName("relative_humidity_2m")]
+    public double[]? RelativeHumidity2M { get; set; }
 }
