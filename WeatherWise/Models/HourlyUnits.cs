5d6bf6d3810b94f2dbc8eff3b09b767faf24d42b
Enej <68961385+menej@users.noreply.github.com>
2024-01-07 14:01:06 +0100

    feat: Add another Data Transfer Object

---
+++ WeatherWise/Models/HourlyUnits.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherWise.Models;
+
+public class HourlyUnits
+{
+    [JsonPropertyName("time")]
+    public string? TimeUnit { get; set; }
+
+    [JsonPropertyName("temperature_2m")]
+    public string? Temperature2MUnit { get; set; }
+    
+    [JsonPropertyName("apparent_temperature")]
+    public string? ApparentTemperatureUnit { get; set; }
+    
+    [JsonPropertyName("relative_humidity_2m")]
+    public string? RelativeHumidity2MUnit { get; set; }
+}
