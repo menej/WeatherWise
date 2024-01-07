13c530c3f780af14c08596c7403633f5d97976db
Enej <68961385+menej@users.noreply.github.com>
2024-01-07 14:02:27 +0100

    feat: Update DTOs properties with a new added DTO HourlyUnits

---
+++ WeatherWise/UserInterface/ConsoleHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace WeatherWise.UserInterface;
+
+public class ConsoleHelper
+{
+    public static void WaitForKeyPress()
+    {
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey(true);
+
+        // Clear the "Press any key to continue." line
+        Console.SetCursorPosition(0, Console.CursorTop - 1);
+        ClearCurrentConsoleLine();        
+    }
+
+    private static void ClearCurrentConsoleLine()
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth)); 
+        Console.SetCursorPosition(0, currentLineCursor);
+    }
+}
